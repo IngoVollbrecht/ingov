a6f26b1ef012d50ba3bf69f32c5506ca4d2c4740
Ingo Aleksander <ingoaleksander@MAC-17661.local>
2020-02-18 13:15:55 -0300

    chore: first commit

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '642b78cc91b0c01248045c9d8166f5bc',
+  expiresIn: '7d',
+};
---
+++ src/config/mailer.js
@@ -0,0 +1,20 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '3106a7980c4806',
+    pass: '6857a302a7c72e',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
+
+/**
+ * Amazon SES
+ * Mailgun
+ * Sparkpost
+ * Mandril (Mailchimp Only)
+ * Do not use SMTP Server from mailling companies (gmail, yahooo, etc)
+ */
